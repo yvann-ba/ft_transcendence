8929ff43d7dac8a87627f76107a9e3c8472bfeac
romainguign <guignard.romain@protonmail.com>
2025-01-16 19:14:23 +0100

    SPA fixed, pong game set-up

--- docker-compose/requirements/app/my_app/views.py
+++ docker-compose/requirements/app/my_app/views.py
@@ -1,12 +1,15 @@
+# views.py
 from django.shortcuts import render
 from .models import Game, User
 
-def game_page(request, page=None):
-    if not page:
-        page = 'home'
-
-    # Exemple d'utilisation des modèles
+def game_page(request, page='home'):  # 'home' est la valeur par défaut
+    # Récupération des données
     games = Game.objects.all()
     users = User.objects.all()
+
+    # Assurez-vous que le template existe avant de le rendre
+    valid_pages = ['home', 'profile', 'settings', 'pong-game', 'about']  # Liste des pages valides
+    if page not in valid_pages:
+        page = '404' 
 
     return render(request, f'{page}.html', {'games': games, 'users': users})
