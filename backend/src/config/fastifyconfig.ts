5e9ad8dedfe16feefba0f90b23f9c60c1b7d5d67
yvann-ba <barbot.yvann@gmail.com>
2025-04-01 10:40:36 +0200

    remove default jwt

--- backend/src/config/fastifyconfig.ts
+++ backend/src/config/fastifyconfig.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  jwtSecret: process.env.JWT_SECRET || 'defaultSecret',
+  jwtSecret: process.env.JWT_SECRET,
   dbPath: process.env.DB_PATH || './database/db.sqlite',
   googleClientId: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
